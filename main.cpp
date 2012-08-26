c05ff366f51d197ab5f8cf4bdaff957ac78c6dc3
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2012-08-26 22:25:28 +0200

    inutile

--- main.cpp
+++ main.cpp
@@ -487,7 +487,6 @@ int main () {
     myfile.close();
     */
 
-    //traceabilities.image();
     //traceabilities.sortByOrderName();
     //traceabilities.sortByWorkstationName();
     traceabilities.sortByWorkstationName_AND_OrderName();
